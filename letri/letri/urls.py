f27a046ced97caabbfd2c91d445cc70ea7a3e101
jfeldman777 <jfeldman777@gmail.com>
2023-02-01 20:58:46 +0200

    includes

--- letri/letri/urls.py
+++ letri/letri/urls.py
@@ -14,8 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('agents/', include('django.contrib.auth.urls')),
+    path('agents/', include('agents.urls')),
+    path('', include('main.urls')),
 ]
---
+++ letri/main/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path, include
+from . import views
+
+urlpatterns = [
+#int str path  slug UUID
+    #path('',views.home,name='home'),
+]
