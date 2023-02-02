283fba67c209547f6d12bacf95b21438c8f101d6
jfeldman777 <jfeldman777@gmail.com>
2023-02-02 12:57:57 +0200

    fields

--- letri/main/admin.py
+++ letri/main/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
-from .models import Region, Army, Body
+from .models import Region, Army, Body, Rang, Ethnos, Education, MobiType
 # Register your models here.
 admin.site.register(Region)
 admin.site.register(Army)
 admin.site.register(Body)
+admin.site.register(Education)
+admin.site.register(MobiType)
+admin.site.register(Rang)
+admin.site.register(Ethnos)
--- letri/main/models.py
+++ letri/main/models.py
@@ -2,6 +2,27 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Ethnos(models.Model):
+    name = models.CharField(max_length=15)
+    def __str__(self):
+        return self.name + ' (' + str(self.id) + ')'
+
+class Education(models.Model):
+    name = models.CharField(max_length=15)
+    def __str__(self):
+        return self.name + ' (' + str(self.id) + ')'
+
+class MobiType(models.Model):
+    name = models.CharField(max_length=15)
+    def __str__(self):
+        return self.name + ' (' + str(self.id) + ')'
+
+class Rang(models.Model):
+    name = models.CharField(max_length=15)
+    def __str__(self):
+        return self.name + ' (' + str(self.id) + ')'
+
 class Region(models.Model):
     name = models.CharField(max_length=15)
     def __str__(self):
@@ -20,6 +41,21 @@ class Body(models.Model):
     army = models.ForeignKey(Army,null=True,on_delete=models.SET_NULL)
     creator = models.ForeignKey(User,blank=True,null=True,on_delete=models.CASCADE)
     image = models.ImageField(null=True,blank=True,upload_to='images/')
+
+    education = models.ForeignKey(Education,null=True,on_delete=models.SET_NULL)
+    mobitype = models.ForeignKey(MobiType,null=True,on_delete=models.SET_NULL)
+    rang = models.ForeignKey(Rang,null=True,on_delete=models.SET_NULL)
+    ethnos = models.ForeignKey(Ethnos,null=True,on_delete=models.SET_NULL)
+
+    oririgin_img = models.ImageField('копия экрана',null=True,blank=True,upload_to='images/')
+    origin_url = models.URLField('линк-источник',blank=True,null=True,)
+
+    flag_done = models.BooleanField('проверено',default=False)
+
+    birth_date = models.DateField('дата рождения',blank=True,null=True,)
+    death_date = models.DateField('дата смерти',blank=True,null=True,)
+
+
 
     def __str__(self):
         return self.first_name + ' ' + self.middle_name+ ' ' + self.last_name + ' (' + str(self.id) + ')'
