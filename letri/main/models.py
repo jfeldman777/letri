5e57d4f8f72ade6a4b7beac3c9972b56af6ee111
jfeldman777 <jfeldman777@gmail.com>
2023-02-02 07:21:02 +0200

    army body region

--- letri/main/models.py
+++ letri/main/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Region(models.Model):
+    name = models.CharField(max_length=15)
+    def __str__(self):
+        return self.name + ' (' + str(self.id) + ')'
+
+class Army(models.Model):
+    name = models.CharField(max_length=15)
+    def __str__(self):
+        return self.name + ' (' + str(self.id) + ')'
+
+class Body(models.Model):
+    first_name = models.CharField(max_length=20)
+    middle_name = models.CharField(max_length=20,blank=True,null=True)
+    last_name = models.CharField(max_length=20)
+    region = models.ForeignKey(Region,null=True,on_delete=models.SET_NULL)
+    army = models.ForeignKey(Army,null=True,on_delete=models.SET_NULL)
+
+    def __str__(self):
+        return self.first_name + ' ' + self.middle_name+ ' ' + self.last_name + ' (' + str(self.id) + ')'
